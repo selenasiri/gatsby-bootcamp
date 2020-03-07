2d12797c3be7279c3d22632e7c72ec24d56d14ec
Selena Siri <selenasiri@gmail.com>
2020-03-06 23:49:49 -0500

    3/6/20

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,5 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
+//configure the plug-ins we want to use
 
 module.exports = {
-  /* Your site config here */
+  plugins: ["gatsby-plugin-sass"],
 }
